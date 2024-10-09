f14855a19c94241f9f515c0bb2385ea10163e05f
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-09 12:07:38 -0300

    merda

--- server/Models/Artista.js
+++ server/Models/Artista.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 
 const artistaSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId },
-    name: { type: String, required: true }
+    name: { type: String, required: true },
+    genero: {type: String}
 })
 
 const artista = mongoose.model('artistas', artistaSchema)
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react"
 import CardSidebar from "./Componentes/CardSidebar"
 import Container from "./Componentes/Container"
 import Header from "./Componentes/Header"
 import Main from "./Componentes/Main"
 import Sidebar from "./Componentes/Sidebar"
+import { useEffect } from "react"
 
 function App() {
+
+  const [artistas, setArtistas] = useState([])
+  useEffect(() => {
+
+    fetch('http://localhost:3000/artistas')
+    .then(res => res.json())
+    .then(res => setArtistas(res))
+    .catch (err => console.log(err))
+    .finally(() => console.log("Acabou!"))
+  }, [])
 
   return (
     <>
@@ -15,7 +27,19 @@ function App() {
           <CardSidebar/>
           <CardSidebar/>
         </Sidebar>
-        <Main />
+        <Main>
+          {
+            artistas
+            .filter(artista => artista.genero === "mandelao")
+            .map(artista => (
+              <div className="bg-red-500 w-28 h-28 flex flex-col justify-around items-center">
+    
+              <p>{artista.name}</p>
+            </div>
+            ))
+          }
+        
+        </Main>
       </Container>
      
    
