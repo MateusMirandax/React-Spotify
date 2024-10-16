b7b9add388e619877bbcc62271b19b6b76f93183
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-16 12:08:10 -0300

    banco

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,11 @@ import Header from "./Componentes/Header"
 import Main from "./Componentes/Main"
 import Sidebar from "./Componentes/Sidebar"
 import { useEffect } from "react"
+import { Outlet } from "react-router-dom"
 
 function App() {
 
-  const [artistas, setArtistas] = useState([])
-  useEffect(() => {
 
-    fetch('http://localhost:3000/artistas')
-    .then(res => res.json())
-    .then(res => setArtistas(res))
-    .catch (err => console.log(err))
-    .finally(() => console.log("Acabou!"))
-  }, [])
 
   return (
     <>
@@ -27,19 +20,9 @@ function App() {
           <CardSidebar/>
           <CardSidebar/>
         </Sidebar>
-        <Main>
-          {
-            artistas
-            .filter(artista => artista.genero === "mandelao")
-            .map(artista => (
-              <div className="bg-red-500 w-28 h-28 flex flex-col justify-around items-center">
-    
-              <p>{artista.name}</p>
-            </div>
-            ))
-          }
-        
-        </Main>
+        <Outlet>
+
+        </Outlet>
       </Container>
      
    
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
 import './index.css'
+import Main from './Componentes/Main.jsx'
+import Artista from './Componentes/Artista.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children: [
+      {index: true, element: <Main/>},
+      {path: '/artistas/:id', element: <Artista/>},
+      {path: '*', element: <h1>PAgeNotFound</h1>}
+    ]
+  }
+])
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
